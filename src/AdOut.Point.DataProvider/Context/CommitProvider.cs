f5f4f669018c21389f23db5e4aa79f2c160159de
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-04-01 18:07:43 +0300

    Commit provider

---
+++ src/AdOut.Point.DataProvider/Context/CommitProvider.cs
@@ -0,0 +1,20 @@
+﻿using AdOut.Point.Model.Interfaces.Context;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdOut.Point.DataProvider.Context
+{
+    public class CommitProvider : ICommitProvider
+    {
+        private readonly IDatabaseContext _context;
+        public CommitProvider(IDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/AdOut.Point.Model/Interfaces/Context/ICommitProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdOut.Point.Model.Interfaces.Context
+{
+    public interface ICommitProvider
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
