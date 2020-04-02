4666c43acbc7250574ff2b285d07fb969138852a
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-04-02 17:41:21 +0300

    Integrated swagger

--- src/AdOut.Point.WebApi/Startup.cs
+++ src/AdOut.Point.WebApi/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AdOut.Point.Core.DI;
 using AdOut.Point.DataProvider.Context;
+using AdOut.Point.DataProvider.DI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace AdOut.Point.WebApi
 {
@@ -30,6 +33,14 @@ namespace AdOut.Point.WebApi
 
             services.AddDbContext<AdPointContext>(options => 
                      options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            services.AddDataProviderModule();
+            services.AddCoreModule();
+
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo { Title = "AdOut.Point API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,6 +55,12 @@ namespace AdOut.Point.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(setup =>
+            {
+                setup.SwaggerEndpoint("/swagger/v1/swagger.json", "AdOut.Point API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
